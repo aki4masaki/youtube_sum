f11680927d75af3e0a12d11ab374ca8a09b5bc47
Aki Shimasaki <akishimasaki@AkinoMacBook-puro.local>
2020-02-19 16:01:15 +0900

    youtube_sum_jp

--- youtube_youyaku/src/Summarizer.py
+++ youtube_youyaku/src/Summarizer.py
@@ -24,7 +24,7 @@ class Summarizer:
                 # New file
 
     def n_distribution(self, n):
-        print("len(self.data)",len(self.data))
+        # print("len(self.data)",len(self.data))
         if len(self.data) == 1:
             return [n]
         else:
@@ -57,15 +57,15 @@ class Summarizer:
         #self.selected_ids[:self.str_len]
         #-> s[:self.str_len * 0.5] で出力長が調整できるか確認
         for j in self.selected_ids[:self.str_len]:
-            print("self.selected_ids",self.selected_ids)
-            print("self.str_len",self.str_len)
+            # print("self.selected_ids",self.selected_ids)
+            # print("self.str_len",self.str_len)
             candidate = self.src_str[j].strip()
             # self.src_str = test_data['src_str']
             if not _block_tri(candidate, _pred):
-                if len(candidate) > 70:
+                if len(candidate) > 68:
                     #_block_tri　三文字連続で_pred後に再出現する文字がある時、candidateを棄却
-                    print("candidate",candidate)
-                    print("len(candidate)",len(candidate))
+                    # print("candidate",candidate)
+                    # print("len(candidate)",len(candidate))
                     _pred.append(candidate)
 
             if (len(_pred) == n) or (n==0):
@@ -89,7 +89,7 @@ class Summarizer:
 
     def _evaluate(self, test_data):
         self.model.eval()
-        print("test_data",test_data)
+        # print("test_data",test_data)
         with torch.no_grad():
             src = torch.tensor([test_data['src']])
             segs = torch.tensor([test_data['segs']])
@@ -102,10 +102,10 @@ class Summarizer:
 
             sent_scores = sent_scores + mask.float()
             selected_ids = torch.argsort(-sent_scores, 1)
-            print("_evaluate.selected_ids",selected_ids)
+            # print("_evaluate.selected_ids",selected_ids)
             selected_ids = selected_ids.cpu().data.numpy()
         self.selected_ids = selected_ids[0]
-        print("test_data",test_data)
+        # print("test_data",test_data)
         self.src_str = test_data['src_str']
         self.str_len = len(test_data['src_str'])
         self.fname = test_data['fname']
